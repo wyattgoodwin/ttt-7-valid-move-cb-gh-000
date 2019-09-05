f906f1bb13ecc9ce5f3464e72b1fda830769268f
Wyatt Goodwin <wog6@cornell.edu>
2019-09-05 19:59:24 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,13 @@
 # code your #valid_move? method here
 def valid_move?(board,index)
   if index.between(0, 8)
-    position_taken?(board, index)
+    if position_taken?(board, index) == false
+      true
+    else
+      false
+    end
   else
-    puts "invalid move!"
+    false
   end
 end
 
